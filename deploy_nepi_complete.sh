3c1061d3971b2c5128215cce177fcb3ab1e1ee48
Jason Seawall <jseawall@numurus.com>
2025-03-07 08:56:45 -0800

    Driver Updates

--- deploy_nepi_complete.sh
+++ deploy_nepi_complete.sh
@@ -27,6 +27,8 @@
 #    NEPI_TARGET_SRC_DIR: Directory to deploy source code to (except _nepi_rui_, which must be located 
 #                         at _/opt/nepi/nepi_rui_ as described in that submodule's README)
 #######################################################################################################
+DEPLOY_3RD_PARTY= false
+
 if [[ -z "${NEPI_REMOTE_SETUP}" ]]; then
   echo "Must have environtment variable NEPI_REMOTE_SETUP set"
   exit 1
@@ -61,7 +63,9 @@ elif [ "${NEPI_REMOTE_SETUP}" == "1" ]; then
   --exclude .git \
   --exclude .gitmodules \
   --exclude .catkin_tools/profiles/*/packages \
+  --exclude src/nepi_3rd_party \
   --exclude devel_* --exclude logs_* --exclude install_* "
+
 
   echo "Excluding ${RSYNC_EXCLUDES}"
 
@@ -77,5 +81,25 @@ elif [ "${NEPI_REMOTE_SETUP}" == "1" ]; then
 
   # Push everything but the EXCLUDES to the specified source folder on the target
   rsync -avzhe "ssh -i ${NEPI_SSH_KEY} -o StrictHostKeyChecking=no"  ${RSYNC_EXCLUDES} ../nepi_engine_ws/ ${NEPI_TARGET_USERNAME}@${NEPI_TARGET_IP}:${NEPI_TARGET_SRC_DIR}/nepi_engine_ws
+
+  if [ "$DEPLOY_3RD_PARTY" = true ]; then
+
+    # Avoid pushing local build artifacts, git stuff, and a bunch of huge GPSD stuff
+    RSYNC_EXCLUDES=" --exclude pc_deploy_nepi_engine_complete.sh \
+    --exclude .git \
+    --exclude .gitmodules \
+    --exclude .catkin_tools/profiles/*/packages \
+    --exclude devel_* --exclude logs_* --exclude install_* "
+
+    echo "Deploying nepi 3rd party repos"
+
+    # Push everything but the EXCLUDES to the specified source folder on the target
+    rsync -avzhe "ssh -i ${NEPI_SSH_KEY} -o StrictHostKeyChecking=no" ${RSYNC_EXCLUDES} ../nepi_engine_ws/src/nepi_3rd_party/ ${NEPI_TARGET_USERNAME}@${NEPI_TARGET_IP}:${NEPI_TARGET_SRC_DIR}/nepi_engine_ws/src/nepi_3rd_party
+  else
+    echo ""
+    echo "Skipping nepi 3rd party repos"
+    echo ""
+  fi
+
 
 fi
