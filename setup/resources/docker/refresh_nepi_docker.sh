6fba97fa8269bda49e3bb70dde748a763d2aea98
nepidev <nepi_github@numurus.com>
2025-08-28 15:39:24 -0700

    **

--- setup/resources/docker/refresh_nepi_docker.sh
+++ setup/resources/docker/refresh_nepi_docker.sh
@@ -10,17 +10,45 @@
 ##
 
 
-# This file Switches a Running Containers
-
-source /home/${USER}/NEPI_CONFIG.sh
+# This File Updates Variables from Docker Config
+source /home/${USER}/.nepi_bash_utils
 wait
+NEPI_DOCKER_CONFIG_FILE=/mnt/nepi_config/docker_cfg/nepi_docker_config.yaml
 
 ########################
-# Update NEPI Docker Variables from nepi_docker_config.yaml
-#export NEPI_HW=JETSON
+#export USER_NAME=nepi
+read_yaml_value "USER_NAME" "USER_NAME" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $USER_NAME
+#export DEVICE_ID=device1
+read_yaml_value "DEVICE_ID" "DEVICE_ID" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $DEVICE_ID
+#export HW_TYPE=JETSON
 read_yaml_value "HW_TYPE" "HW_TYPE" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $HW_TYPE
+#export HW_MODEL=ORIN
 read_yaml_value "HW_MODEL" "HW_MODEL" "$NEPI_DOCKER_CONFIG_FILE"
-#echo $NEPI_HW
+#echo $HW_MODEL
+
+#export STATIC_IP=192.168.179.103
+read_yaml_value "STATIC_IP" "STATIC_IP" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $STATIC_IP
+#export IP_ALIASES=[]
+read_yaml_value "IP_ALIASES" "IP_ALIASES" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $IP_ALIASES
+
+#export MANAGES_CLOCK=1
+read_yaml_value "MANAGES_CLOCK" "MANAGES_CLOCK" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $MANAGES_CLOCK
+
+#export SUPPORTS_AB_FS=1
+read_yaml_value "SUPPORTS_AB_FS" "SUPPORTS_AB_FS" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $SUPPORTS_AB_FS
+#IMPORT_PATH=/mnt/nepi_storage/nepi_images
+read_yaml_value "IMPORT_PATH" "IMPORT_PATH" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $IMPORT_PATH
+#EXPORT_PATH=/mnt/nepi_storage/nepi_images
+read_yaml_value "EXPORT_PATH" "EXPORT_PATH" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $EXPORT_PATH
 
 #export ACTIVE_CONT=nepi_fs_a
 read_yaml_value "ACTIVE_CONT" "ACTIVE_CONT" "$NEPI_DOCKER_CONFIG_FILE"
@@ -53,22 +81,14 @@ read_yaml_value "INACTIVE_TAG" "INACTIVE_TAG" "$NEPI_DOCKER_CONFIG_FILE"
 #export INACTIVE_ID=0
 read_yaml_value "INACTIVE_ID" "INACTIVE_ID" "$NEPI_DOCKER_CONFIG_FILE"
 #echo $INACTIVE_ID
-#export STAGING_CONT=nepi_staging
+
+#export STAGING_CONT=nepi_fs_staging
 read_yaml_value "STAGING_CONT" "STAGING_CONT" "$NEPI_DOCKER_CONFIG_FILE"
 #echo $STAGING_CONT
 
-#STAGING_CONT: nepi_staging
-read_yaml_value "STAGING_CONT" "STAGING_CONT" "$NEPI_DOCKER_CONFIG_FILE"
-#echo $STAGING_CONT
-#IMPORT_PATH: /media/nepidev/NServer_Backup
-read_yaml_value "IMPORT_PATH" "IMPORT_PATH" "$NEPI_DOCKER_CONFIG_FILE"
-#echo $IMPORT_PATH
-#EXPORT_PATH: /mnt/nepi_storage/nepi_full_img
-read_yaml_value "EXPORT_PATH" "EXPORT_PATH" "$NEPI_DOCKER_CONFIG_FILE"
-#echo $EXPORT_PATH
-
-
-######  NEED TO: Update from current docker status
+#export RUNNING=0
+read_yaml_value "RUNNING" "RUNNING" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $RUNNING
 #export RUNNING_CONT=None
 read_yaml_value "RUNNING_CONT" "RUNNING_CONT" "$NEPI_DOCKER_CONFIG_FILE"
 #echo $RUNNING_CONT
@@ -86,7 +106,11 @@ read_yaml_value "RUNNING_ID" "RUNNING_ID" "$NEPI_DOCKER_CONFIG_FILE"
 read_yaml_value "NEPI_REMOTE_SETUP" "NEPI_REMOTE_SETUP" "$NEPI_DOCKER_CONFIG_FILE"
 #echo $NEPI_REMOTE_SETUP
 
+#export MAX_COUNT=3
+read_yaml_value "MAX_COUNT" "MAX_COUNT" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $MAX_COUNT
+#export FAIL_COUNT=0
+read_yaml_value "FAIL_COUNT" "FAIL_COUNT" "$NEPI_DOCKER_CONFIG_FILE"
+#echo $FAIL_COUNT
+
 ########################
-
-
-
