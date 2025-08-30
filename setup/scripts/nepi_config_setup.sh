edae990972143531c27a3af19f20b74a42077351
Jason Seawall <jseawall@numurus.com>
2025-08-30 14:23:55 -0700

    **

--- setup/scripts/nepi_config_setup.sh
+++ setup/scripts/nepi_config_setup.sh
@@ -125,6 +125,76 @@ echo "NEPI_ALIASES_FILE: ${NEPI_ALIASES_FILE}" >> $CONFIG_DEST
 echo "NEPI_AB_FS: ${NEPI_AB_FS}" >> $CONFIG_DEST
 
 
+echo "Adding NEPI Docker variables to nepi config file ${CONFIG_DEST}"
+
+echo "# NEPI Docker Docker System Variables" >> $CONFIG_DEST
+echo "NEPI_ACTIVE_NAME: nepi_fs_a" >> $CONFIG_DEST
+echo "NEPI_ACTIVE_VERSION: 3p2p0" >> $CONFIG_DEST
+echo "NEPI_ACTIVE_TAG: ${NEPI_HW_TYPE}-${ACTIVE_VERSION}}" >> $CONFIG_DEST
+echo "NEPI_ACTIVE_ID: 0" >> $CONFIG_DEST
+echo "NEPI_ACTIVE_LABEL: uknown" >> $CONFIG_DEST
+
+
+echo "NEPI_INACTIVE_NAME: nepi_fs_b" >> $CONFIG_DEST
+echo "NEPI_INACTIVE_VERSION: uknown" >> $CONFIG_DEST
+echo "NEPI_INACTIVE_TAG: ${NEPI_HW_TYPE}-${INACTIVE_VERSION}" >> $CONFIG_DEST
+echo "NEPI_INACTIVE_ID: 0" >> $CONFIG_DEST
+echo "NEPI_INACTIVE_LABEL: uknown" >> $CONFIG_DEST
+
+echo "NEPI_STAGING_NAME: nepi_fs_staging" >> $CONFIG_DEST
+
+echo "# Running NEPI Container Info" >> $CONFIG_DEST
+echo "NEPI_RUNNING: 0" >> $CONFIG_DEST
+echo "NEPI_RUNNING_NAME: None" >> $CONFIG_DEST
+echo "NEPI_RUNNING_NAME: uknown" >> $CONFIG_DEST
+echo "NEPI_RUNNING_VERSION: uknown" >> $CONFIG_DEST
+echo "NEPI_RUNNING_TAG: uknown" >> $CONFIG_DEST
+echo "NEPI_RUNNING_ID: 0" >> $CONFIG_DEST
+
+echo "# Boot Fail Config" >> $CONFIG_DEST
+echo "NEPI_MAX_COUNT: 3" >> $CONFIG_DEST
+echo "NEPI_FAIL_COUNT: 0" >> $CONFIG_DEST
+
+#######################
+# Copy the nepi_config.yaml file to the factory_cfg folder
+factory_config=${NEPI_CONFIG}/factory_cfg/etc/nepi_config.yaml
+echo "Updating NEPI System Config Files in ${factory_config}"
+if [ ! -d "${NEPI_CONFIG}" ]; then
+    sudo sudo mkdir $NEPI_CONFIG
+fi
+if [ ! -d "${NEPI_CONFIG}/factory_cfg" ]; then
+    sudo mkdir ${NEPI_CONFIG}/factory_cfg
+fi
+if [ ! -d "${NEPI_CONFIG}/factory_cfg/etc" ]; then
+    sudo mkdir ${NEPI_CONFIG}/factory_cfg/etc
+fi
+
+#if [ -f "$factory_config" ]; then
+#    sudo cp $factory_config ${factory_config}.bak
+#fi
+echo "Copying NEPI System Config File ${CONFIG_DEST} to ${factory_config}"
+sudo cp ${CONFIG_DEST} ${factory_config}
+sudo chown -R ${USER}:${USER} $NEPI_CONFIG
+
+# Copy the nepi_config.yaml file to the system_cfg folder
+sys_config=${NEPI_CONFIG}/system_cfg/etc/nepi_config.yaml
+echo "Updating NEPI System Config Files in ${sys_config}"
+if [ ! -d "${NEPI_CONFIG}/system_cfg/etc" ]; then
+    sudo sudo mkdir $NEPI_CONFIG
+fi
+if [ ! -d "${NEPI_CONFIG}/system_cfg" ]; then
+    sudo mkdir ${NEPI_CONFIG}/system_cfg
+fi
+if [ ! -d "${NEPI_CONFIG}/system_cfg/etc" ]; then
+    sudo mkdir ${NEPI_CONFIG}/system_cfg/etc
+fi
+
+#if [ -f "$sys_config" ]; then
+#    sudo cp $sys_config ${sys_config}.bak
+#fi
+echo "Copying NEPI System Config File ${CONFIG_DEST} to ${sys_config}"
+sudo cp ${CONFIG_DEST} ${sys_config}
+sudo chown -R ${USER}:${USER} $NEPI_CONFIG
 
 
 # Update NEPI_FOLDER owners
