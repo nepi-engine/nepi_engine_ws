63646151fb549b91ca0e51a7c138b2159add85d4
jseawall@numurus.com <jseawall@numurus.com>
2026-07-09 11:35:39 -0700

    **

--- build_nepi_rui.sh
+++ build_nepi_rui.sh
@@ -96,23 +96,6 @@ sudo rsync -arp ${SCRIPT_FOLDER}/src/nepi_rui/ ${NEPI_BASE}/nepi_rui/
 sudo chmod 775 -R ${NEPI_BASE}/nepi_rui/src/rui_webserver/rui-app/src
 echo "NEPI RUI Deploy Finished"
 
-system_source_path="${NEPI_CONFIG}/system_cfg/nepi_rui"
-rui_build_folder=${NEPI_RUI}/src/rui_webserver/rui-app/src
-echo "Updating NEPI RUI source from system config folder ${system_source_path}"
-
-
-system_source_path="${NEPI_CONFIG}/system_cfg/src/nepi_rui"
-rui_build_folder=${NEPI_RUI}/src/rui_webserver/rui-app/src
-echo "Updating NEPI RUI source from system config folder ${system_source_path}"
-
-if [[ -d $system_source_path ]]; then
-      echo "Copying system src files from ${system_source_path} to ${rui_build_folder}"
-      if [[ -d $rui_build_folder ]]; then
-        sudo cp -r -p ${system_source_path}/* ${rui_build_folder}/
-        sudo chown -R ${CONFIG_USER}:${CONFIG_USER} $rui_build_folder
-        echo "Copied system src files from ${system_source_path} to ${rui_build_folder}"
-      fi
-fi
 
 
 echo ""
@@ -162,6 +145,21 @@ echo ""
 
 
 ##################
+
+system_source_path="${NEPI_CONFIG}/system_cfg/src/nepi_rui"
+rui_build_folder=${NEPI_RUI}/src/rui_webserver/rui-app/src
+echo "Updating NEPI RUI source from system config folder ${system_source_path}"
+
+if [[ -d $system_source_path ]]; then
+      echo "Copying system src files from ${system_source_path} to ${rui_build_folder}"
+      if [[ -d $rui_build_folder ]]; then
+        sudo cp -r -p ${system_source_path}/* ${rui_build_folder}/
+        sudo chown -R ${CONFIG_USER}:${CONFIG_USER} $rui_build_folder
+        echo "Copied system src files from ${system_source_path} to ${rui_build_folder}"
+      fi
+fi
+
+
 system_rui_config="${NEPI_CONFIG}/system_cfg/nepi_rui"
 build_rui_folder="${NEPI_BASE}/nepi_rui/src/rui_webserver/rui-app/src"
 echo "Updating RUI source from system config folder ${system_rui_config}"
